d973f54fed055f73556b3ea21d35d3389ca714c6
Gle Bang <101760474+Gwithajoy@users.noreply.github.com>
2023-10-19 22:18:02 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -2,13 +2,7 @@ t = int(input())
 
 for _ in range(t):
   c = int(input())
-  q = c // 25
-  c = c - (q * 25)
-  d = c // 10
-  c = c - (d * 10)
-  n = c // 5
-  c = c - (n * 5)
-  p = c // 1
-  
-
-  print(q, d, n, p)
+  for i in [25, 10, 5, 1]:
+    print(c // i, end=' ')
+    c = c % i
+ 
